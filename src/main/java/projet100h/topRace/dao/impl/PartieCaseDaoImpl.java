41c8bad14b742e9716e74d798735812a3db3eb3a
Camlgl <camille.le-gal@hei.yncrea.fr>
2018-02-20 15:29:00 +0100

    fonction modifierStatut en SQL

--- src/main/java/projet100h/topRace/dao/impl/PartieCaseDaoImpl.java
+++ src/main/java/projet100h/topRace/dao/impl/PartieCaseDaoImpl.java
@@ -15,7 +15,7 @@ public class PartieCaseDaoImpl implements PartieCaseDao {
              PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setInt(1, x);
             statement.setString(2, String.valueOf(y));
-            statement.setInt(2, idPartie);
+            statement.setInt(3, idPartie);
             try (ResultSet resultSet = statement.executeQuery()) {
                 if (resultSet.next()) {
                     return new PartieCase(resultSet.getInt("x"),
@@ -28,6 +28,23 @@ public class PartieCaseDaoImpl implements PartieCaseDao {
             e.printStackTrace();
         }
         return null;
+    }
+
+
+    public void modifierStatut(int x, char y, int idPartie, boolean occupe){
+        String query = "UPDATE partieCase SET occupee=? WHERE x=? and y=? and idPartie=?";
+        try (Connection connection = DataSourceProvider.getDataSource().getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setBoolean(1,occupe);
+            statement.setInt(2, x);
+            statement.setString(3, String.valueOf(y));
+            statement.setInt(4, idPartie);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    
     }
 
 }
