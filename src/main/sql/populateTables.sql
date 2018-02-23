41c579d2448bdba7af8e5c53b1988f49e478b12a
Thibeo <thibaut.leplat@hei.yncrea.fr>
2018-02-23 01:03:03 +0100

    plein de nouveautés

--- src/main/sql/populateTables.sql
+++ src/main/sql/populateTables.sql
@@ -443,6 +443,8 @@ INSERT INTO `carte` (`idCarte`) VALUES (18);
 INSERT INTO `carte` (`idCarte`) VALUES (19);
 INSERT INTO `carte` (`idCarte`) VALUES (20);
 
+/* LES CARTES */
+
 /* initialisation */
 INSERT INTO `nbCase` (`couleurV`, `idCarte`, `nbCase`) VALUES ("Jaune",0,0);
 INSERT INTO `nbCase` (`couleurV`, `idCarte`, `nbCase`) VALUES ("Rouge",0,0);
@@ -458,6 +460,28 @@ INSERT INTO `nbCase` (`couleurV`, `idCarte`, `nbCase`) VALUES ("Violet",3,10);
 INSERT INTO `nbCase` (`couleurV`, `idCarte`, `nbCase`) VALUES ("Bleu",4,10);
 INSERT INTO `nbCase` (`couleurV`, `idCarte`, `nbCase`) VALUES ("Vert",5,10);
 INSERT INTO `nbCase` (`couleurV`, `idCarte`, `nbCase`) VALUES ("Blanc",6,10);
+
 /* 5 - 2 */
+/* 1er */
 INSERT INTO `nbCase` (`couleurV`, `idCarte`, `nbCase`) VALUES ("Jaune",7,5);
 INSERT INTO `nbCase` (`couleurV`, `idCarte`, `nbCase`) VALUES ("Violet",7,2);
+
+/* 6 - 4 - 2 - 1 */
+/* 1er */
+INSERT INTO `nbCase` (`couleurV`, `idCarte`, `nbCase`) VALUES ("Bleu",8,6);
+INSERT INTO `nbCase` (`couleurV`, `idCarte`, `nbCase`) VALUES ("Blanc",8,4);
+INSERT INTO `nbCase` (`couleurV`, `idCarte`, `nbCase`) VALUES ("Vert",8,2);
+INSERT INTO `nbCase` (`couleurV`, `idCarte`, `nbCase`) VALUES ("Rouge",8,1);
+
+/* 6 - 5 - 4 - 3 - 2 - 1 */
+/* 1er */
+INSERT INTO `nbCase` (`couleurV`, `idCarte`, `nbCase`) VALUES ("Vert",9,6);
+INSERT INTO `nbCase` (`couleurV`, `idCarte`, `nbCase`) VALUES ("Violet",9,5);
+INSERT INTO `nbCase` (`couleurV`, `idCarte`, `nbCase`) VALUES ("Blanc",9,4);
+INSERT INTO `nbCase` (`couleurV`, `idCarte`, `nbCase`) VALUES ("Jaune",9,3);
+INSERT INTO `nbCase` (`couleurV`, `idCarte`, `nbCase`) VALUES ("Bleu",9,2);
+INSERT INTO `nbCase` (`couleurV`, `idCarte`, `nbCase`) VALUES ("Rouge",9,1);
+
+/* 5  */
+/* Rouge */
+INSERT INTO `nbCase` (`couleurV`, `idCarte`, `nbCase`) VALUES ("Rouge",10,5);
